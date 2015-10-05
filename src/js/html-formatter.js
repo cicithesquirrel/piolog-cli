e415932c685d583d0cbdf52c7490cc1e9e140c94
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2015-10-05 08:34:36 +0200

    Calcul des stats

--- src/js/html-formatter.js
+++ src/js/html-formatter.js
@@ -3,24 +3,25 @@
 var log4js = require('log4js');
 var mustache = require('mustache');
 var fs = require('fs');
+var piolog = require('piolog');
 
 var logger = log4js.getLogger("piolog.formatter.html");
 
 exports.format = function (game, options) {
 
-    game.stats = {
-        nthTurnLabel: function () {
-            var numberOfTurns = game.turns.length - 1;
-            var label = '' + numberOfTurns;
-            if (numberOfTurns % 10 === 1) {
-                label = label + 'st';
-            } else if (numberOfTurns % 10 === 2) {
-                label = label + 'nd';
-            } else {
-                label = label + 'th';
-            }
-            return label;
+    game.stats = piolog.computeStats(game);
+
+    game.stats.nthTurnLabel = function () {
+        var numberOfTurns = game.turns.length - 1;
+        var label = '' + numberOfTurns;
+        if (numberOfTurns % 10 === 1) {
+            label = label + 'st';
+        } else if (numberOfTurns % 10 === 2) {
+            label = label + 'nd';
+        } else {
+            label = label + 'th';
         }
+        return label;
     };
 
     var page = fs.readFileSync("./src/html/html-formatter.html", "utf8");
