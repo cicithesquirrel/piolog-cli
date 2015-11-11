2637a9f11d8cf789c45148cdf875761cb5a3598c
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2015-11-11 22:35:16 +0100

    Separated FileReader and ContentBuffer tests

--- test/js/test_filereader.js
+++ test/js/test_filereader.js
@@ -3,223 +3,106 @@
 var test = require('unit.js');
 var filereader = require('../../src/js/filereader');
 
-describe('"filereader" tests', function () {
 
-    describe('"ContentBuffer" tests', function () {
+describe('"FileReader" tests', function () {
 
-        it('New content buffer is empty', function () {
+    it('New file reader is not opened', function () {
 
-            var cb = new filereader.ContentBuffer();
+        var fr = new filereader.FileReader('inexistent_file');
 
-            test.bool(cb.isEmpty()).isTrue();
-        });
-
-        it('Empty buffer size', function () {
-
-            var cb = new filereader.ContentBuffer();
-
-            test.number(cb.size).is(0);
-        });
-
-        it('Empty buffer content', function () {
-
-            var cb = new filereader.ContentBuffer();
-
-            test.string(cb.getTempBufferContent()).is('');
-        });
-
-        it('Append content to empty buffer', function () {
-
-            var cb = new filereader.ContentBuffer();
-
-            cb.append(new Buffer('abc def\nghi jkl '), 16);
-
-            test.number(cb.size).is(16);
-        });
-
-        it('Non-empty buffer is not empty', function () {
-
-            var cb = new filereader.ContentBuffer();
-            cb.append(new Buffer('abc def\nghi jkl '), 16);
-
-            test.bool(cb.isEmpty()).isFalse();
-        });
-
-        it('Non-empty buffer size', function () {
-
-            var cb = new filereader.ContentBuffer();
-            cb.append(new Buffer('abc def\nghi jkl '), 16);
-
-            test.number(cb.size).is(16);
-        });
-
-        it('Append content to non-empty buffer', function () {
-
-            var cb = new filereader.ContentBuffer();
-            cb.append(new Buffer('abc def\nghi jkl '), 16);
-
-            cb.append(new Buffer('mno pqr\nstu vwx '), 16);
-
-            test.number(cb.size).is(32);
-        });
-
-        it('Content of non-empty buffer', function () {
-
-            var cb = new filereader.ContentBuffer();
-            cb.append(new Buffer('abc def\nghi jkl '), 16);
-
-            test.string(cb.getTempBufferContent()).is('abc def\nghi jkl ');
-        });
-
-        it('Get one line of text when buffer contains one', function () {
-
-            var cb = new filereader.ContentBuffer();
-            cb.append(new Buffer('abc def\nghi jkl '), 16);
-
-            test.string(cb.getOneLineFromTempBuffer()).is('abc def');
-
-            test.number(cb.size).is(8);
-        });
-
-        it('Get one line of text when buffer does not contain one', function () {
-
-            var cb = new filereader.ContentBuffer();
-            cb.append(new Buffer('abc def ghi jkl '), 16);
-
-            test.value(cb.getOneLineFromTempBuffer()).isUndefined();
-
-            test.number(cb.size).is(16);
-        });
-
-        it('Get one line of text when buffer is empty', function () {
-
-            var cb = new filereader.ContentBuffer();
-
-            test.value(cb.getOneLineFromTempBuffer()).isUndefined();
-        });
-
-        it('Size of cleared already empty buffer', function () {
-
-            var cb = new filereader.ContentBuffer();
-
-            cb.clear();
-
-            test.number(cb.size).is(0);
-        });
-
-        it('Size of cleared non-empty buffer', function () {
-
-            var cb = new filereader.ContentBuffer();
-            cb.append(new Buffer('abc def ghi jkl '), 16);
-
-            cb.clear();
-
-            test.number(cb.size).is(0);
-        });
-
+        test.bool(fr.isOpened()).isFalse();
     });
 
-    describe('"FileReader" tests', function () {
+    it('Opened file is opened', function () {
 
-        it('New file reader is not opened', function () {
+        var fr = new filereader.FileReader('test/data/empty_file');
+        fr.open();
 
-            var fr = new filereader.FileReader('inexistent_file');
+        test.bool(fr.isOpened()).isTrue();
 
-            test.bool(fr.isOpened()).isFalse();
-        });
-
-        it('Opened file is opened', function () {
-
-            var fr = new filereader.FileReader('test/data/empty_file');
-            fr.open();
-
-            test.bool(fr.isOpened()).isTrue();
-
-            fr.close();
-        });
-
-        it('Closed file is not opened', function () {
-
-            var fr = new filereader.FileReader('test/data/empty_file');
-            fr.open();
-            fr.close();
-
-            test.bool(fr.isOpened()).isFalse();
-        });
-
-        it('File is not EOF when just opened', function () {
-
-            var fr = new filereader.FileReader('test/data/empty_file');
-            fr.open();
-
-            test.bool(fr.isEndOfFile()).isFalse();
-
-            fr.close();
-        });
-
-        it('Read empty file reaches EOF', function () {
-
-            var fr = new filereader.FileReader('test/data/empty_file');
-            fr.open();
-
-            var line = fr.readLine();
-            test.string(line).is('');
-
-            test.bool(fr.isEndOfFile()).isTrue();
-
-            fr.close();
-        });
-
-        it('Read three lines of a file', function () {
-
-            var fr = new filereader.FileReader('test/data/five_lines_file');
-            fr.open();
-
-            var line;
-
-            test.bool(fr.isEndOfFile()).isFalse();
-
-            line = fr.readLine();
-            test.string(line).is('07:20:20 Connexion à localhost, port 5556');
-            test.bool(fr.isEndOfFile()).isFalse();
-
-            line = fr.readLine();
-            test.string(line).is('07:20:20 Le joueur 3 s\'appelle maintenant cyril.');
-            test.bool(fr.isEndOfFile()).isFalse();
-
-            line = fr.readLine();
-            test.string(line).is('07:20:20 Le joueur 1 s\'appelle maintenant Bach.');
-            test.bool(fr.isEndOfFile()).isFalse();
-
-            fr.close();
-        });
-
-        it('Read all lines of a file', function () {
-
-            var fr = new filereader.FileReader('test/data/five_lines_file');
-            fr.open();
-
-            var line;
-
-            line = fr.readLine();
-            test.string(line).is('07:20:20 Connexion à localhost, port 5556');
-
-            line = fr.readLine();
-            test.string(line).is('07:20:20 Le joueur 3 s\'appelle maintenant cyril.');
-
-            line = fr.readLine();
-            test.string(line).is('07:20:20 Le joueur 1 s\'appelle maintenant Bach.');
-
-            line = fr.readLine();
-            test.string(line).is('07:20:20 Le joueur 0 s\'appelle maintenant Coolio.');
-
-            line = fr.readLine();
-            test.string(line).is('07:20:20 Le joueur 2 s\'appelle maintenant Saddam Hussein.');
-
-            test.bool(fr.isEndOfFile()).isTrue();
-
-            fr.close();
-        });
+        fr.close();
     });
 
+    it('Closed file is not opened', function () {
+
+        var fr = new filereader.FileReader('test/data/empty_file');
+        fr.open();
+        fr.close();
+
+        test.bool(fr.isOpened()).isFalse();
+    });
+
+    it('File is not EOF when just opened', function () {
+
+        var fr = new filereader.FileReader('test/data/empty_file');
+        fr.open();
+
+        test.bool(fr.isEndOfFile()).isFalse();
+
+        fr.close();
+    });
+
+    it('Read empty file reaches EOF', function () {
+
+        var fr = new filereader.FileReader('test/data/empty_file');
+        fr.open();
+
+        var line = fr.readLine();
+        test.string(line).is('');
+
+        test.bool(fr.isEndOfFile()).isTrue();
+
+        fr.close();
+    });
+
+    it('Read three lines of a file', function () {
+
+        var fr = new filereader.FileReader('test/data/five_lines_file');
+        fr.open();
+
+        var line;
+
+        test.bool(fr.isEndOfFile()).isFalse();
+
+        line = fr.readLine();
+        test.string(line).is('07:20:20 Connexion à localhost, port 5556');
+        test.bool(fr.isEndOfFile()).isFalse();
+
+        line = fr.readLine();
+        test.string(line).is('07:20:20 Le joueur 3 s\'appelle maintenant cyril.');
+        test.bool(fr.isEndOfFile()).isFalse();
+
+        line = fr.readLine();
+        test.string(line).is('07:20:20 Le joueur 1 s\'appelle maintenant Bach.');
+        test.bool(fr.isEndOfFile()).isFalse();
+
+        fr.close();
+    });
+
+    it('Read all lines of a file', function () {
+
+        var fr = new filereader.FileReader('test/data/five_lines_file');
+        fr.open();
+
+        var line;
+
+        line = fr.readLine();
+        test.string(line).is('07:20:20 Connexion à localhost, port 5556');
+
+        line = fr.readLine();
+        test.string(line).is('07:20:20 Le joueur 3 s\'appelle maintenant cyril.');
+
+        line = fr.readLine();
+        test.string(line).is('07:20:20 Le joueur 1 s\'appelle maintenant Bach.');
+
+        line = fr.readLine();
+        test.string(line).is('07:20:20 Le joueur 0 s\'appelle maintenant Coolio.');
+
+        line = fr.readLine();
+        test.string(line).is('07:20:20 Le joueur 2 s\'appelle maintenant Saddam Hussein.');
+
+        test.bool(fr.isEndOfFile()).isTrue();
+
+        fr.close();
+    });
 });
